81319a98fcb5f5e7bebb2c359dd45d45e75e92ed
Megha Bhakta <meghabhakta3@gmail.com>
2021-02-03 02:28:42 +0530

    Added Menu Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatToolbarModule} from '@angular/material/toolbar';
 import { MatButtonModule} from '@angular/material/button';
 import { MatCardModule} from '@angular/material/card';
 import { MatGridListModule} from '@angular/material/grid-list';
+import { MenuComponent } from './components/menu/menu.component';
 
 
 
@@ -23,7 +24,8 @@ import { MatGridListModule} from '@angular/material/grid-list';
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
